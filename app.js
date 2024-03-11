f7366e179b4df1630e2ca7d44a822c6cd01d138e
00016417 <wiut00016417@gmail.com>
2024-03-11 17:16:52 +0500

    modified files

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const conf = require('dotenv').config
 const body_parser = require('body-parser')
 const path = require('path')
 const express = require('express');
@@ -7,15 +6,14 @@ const web_route = require('./routes/web')
 global.mock_db = path.join(__dirname, './data/mock_db.json');
 
 const app = express();
-const port = process.env.port || 3000;
 
 app.set('view engine', 'pug');
 
-app.use('/css', express.static('public'))
-app.use('/js', express.static('public'))
+app.use('/css', express.static('public/css'))
+app.use('/js', express.static('public/js'))
 
 app.use('/', web_route);
-
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
